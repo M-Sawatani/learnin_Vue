c39587fe901035bb4214dd205ea815e126e48731
M-Sawatani <35281194+M-Sawatani@users.noreply.github.com>
2020-04-08 16:00:56 +0900

    Qiita Real time search作成

---
+++ realtime_search_qiita/js/main.js
@@ -0,0 +1,55 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    items: null, // APIから取得した検索結果格納用
+    keyword: "", // ユーザーが入力した検索キーワード
+    message: "", // ユーザーに表示するメッセージ
+    canClear: false
+  },
+  watch: {
+    keyword: function(newval, oldval) {
+      //   console.log(newval);
+      this.message = "Waiting for you to stop typing...";
+      this.debouncedGetAnswer();
+    }
+  },
+  created: function() {
+    // $elプロパティは利用不可。moutedであれば利用可能。
+    // this.keyword = "JavaScript";
+    // this.getAnswer();
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+  },
+  methods: {
+    getAnswer: function() {
+      if (this.keyword === "") {
+        this.items = null;
+        this.message = "";
+        this.canClear = false;
+        return;
+      }
+      this.canClear = true;
+      this.message = "Loading...";
+      var vm = this;
+      var params = { page: 1, per_page: 20, query: this.keyword };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then(function(resp) {
+          vm.message = "Result:";
+          console.log(resp);
+          vm.items = resp.data;
+        })
+        .catch(function(error) {
+          vm.message = `Error!! ${error}`;
+        })
+        .finally(function() {
+          vm.messge = "";
+        });
+    },
+    deleteKeyword: function() {
+      this.keyword = "";
+      this.items = null;
+      this.message = "";
+      this.canClear = false;
+    }
+  }
+});
